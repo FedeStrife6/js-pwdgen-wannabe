63ef34a624bb83490d59aa1e668202ff3f5df8d8
fedestrife <federc.prc@gmail.com>
2020-05-19 16:04:28 +0200

    Non ho concluso

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var nome = prompt('Inserisci il tuo nome');
+
+var eta = prompt('Inserisci il tuo cognome');
+
+var colore = prompt('Inserisci il tuo colore preferito');
+
+var password = prompt( nome + eta + colore + ' 19' );
+document.getElementById('passwordFacile').innerHTML = password;
