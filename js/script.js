cc179a3eb03cd226bc2f7e4a0b41d0577fc84cc1
fedestrife <federc.prc@gmail.com>
2020-05-19 16:17:40 +0200

    corretto

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
+// Informazioni Richieste All'Utente //
 var nome = prompt('Inserisci il tuo nome');
-
 var eta = prompt('Inserisci il tuo cognome');
-
 var colore = prompt('Inserisci il tuo colore preferito');
 
-var password = prompt( nome + eta + colore + ' 19' );
-document.getElementById('passwordFacile').innerHTML = password;
+// Testo Da Visualizzare Una Volta Inserite Le Info //
+var password = 'Benvenuto ' + nome + eta + colore + ' 19. ' ;
+document.getElementById('password').innerHTML = password;
+
+//console.log(password);//
