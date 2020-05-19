aab6aaf1740dcab934288d58414c3a9337a9829f
fedestrife <federc.prc@gmail.com>
2020-05-19 16:32:40 +0200

    Piccola correzione

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ var eta = prompt('Inserisci il tuo cognome');
 var colore = prompt('Inserisci il tuo colore preferito');
 
 // Testo Da Visualizzare Una Volta Inserite Le Info //
-var password = 'Benvenuto ' + nome + eta + colore + '19. ' ;
-document.getElementById('password').innerHTML = password;
+var password =  nome + eta + colore + '19';
+document.getElementById('password').innerHTML = 'Benvenuto ' + password + '.';
 
 //console.log(password);//
